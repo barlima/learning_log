77a2e1f06eca099edb7105e26e1c886e454d5ca2
barlima <bartekperucki@gmail.com>
2017-10-16 17:57:30 +0200

    users login and sing up

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'learning_logs'
+# app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def logout_view(request):
+    """User logout"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+
+def register(request):
+    """Register new user"""
+    if request.method != 'POST':
+        # Display an empty form
+        form = UserCreationForm()
+    else:
+        # Render a form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # login in user and redirect him to home page
+            authenticated_user = authenticate(username=new_user.username,
+                                              password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
